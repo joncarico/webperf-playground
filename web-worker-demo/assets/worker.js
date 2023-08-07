14085aafff4f5d380269de4b4c1705c27a75b71c
Jon Carico <carico.jon@gmail.com>
2023-08-07 09:48:16 -0700

    update

--- web-worker-demo/assets/worker.js
+++ web-worker-demo/assets/worker.js
@@ -1,14 +1,14 @@
-self.onmessage = function(event) {
-  const scriptUrls = event.data;
-
-  // Load and execute each script
-  scriptUrls.forEach(function(url) {
-    importScript(url);
-  });
-};
-
-function importScript(url) {
-  const script = document.createElement('script');
-  script.src = url;
-  document.head.appendChild(script);
-}
+(() => {
+	self.addEventListener('message', evt => {
+		if (evt.data.action === 'actions') {
+			evt.data.value.forEach(action => {
+				importScripts(action);
+			});
+		} else {
+			self.postMessage({
+				value: self[evt.data.action](evt.data.value),
+				action: evt.data.action,
+			});
+		}
+	});
+})();
